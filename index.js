456233ba5bea84dc10ba3ab5514b4e3c926d9f13
elisavet12 <85793200+elisavet12@users.noreply.github.com>
2022-06-28 18:16:42 +0300

    almost there...

--- index.js
+++ index.js
@@ -3,22 +3,17 @@ var close = document.querySelector("button.close");
 var nav = document.querySelector("nav.nav--main");
 var body = document.querySelector("body");
 var menu = document.querySelector("nav__link-actions-mb");
-// var elsa = document.querySelector("div.header__nav");
 
 function openMenu() {
   nav.classList.add("nav--open");
   body.classList.add("noScroll");
   menu.classList.add("nav__link-actions");
-  // burger.classList.remove("burger");
-  // close.classList.add("close");
 }
 
 function closeMenu() {
   nav.classList.remove("nav--open");
   body.classList.remove("noScroll");
   menu.classList.remove("nav__link-actions");
-  // burger.classList.add("burger");
-  // close.classList.remove("close");
 }
 
 function isMobile() {
@@ -27,5 +22,18 @@ function isMobile() {
   }
 }
 
+// function myFunction(x) {
+//   x.classList.toggle("close");
+// }
+
 burger.addEventListener("click", openMenu);
 close.addEventListener("click", closeMenu);
+
+burger.addEventListener("click", function handleClick() {
+  if ((burger.style.display = "none")) {
+    close.style.display = "block";
+  } else {
+    burger.style.display = "none";
+    close.style.display = "block";
+  }
+});
